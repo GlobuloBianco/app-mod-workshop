2429d973cbcd7023a83d4bce94949af621eb7f7a
GlobuloBianco <wufrancescojh@gmail.com>
2025-03-20 15:33:33 +0000

    rebuild

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $db_name = getenv('DB_NAME') ?: 'image_catalog';
 $db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
 $db_pass = getenv('DB_PASS') ?: 'wrong_password';
 // Note getenv() is PHP 5.3 compatible
-try {
+try { 
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
