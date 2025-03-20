996aaeab3876830bda6e11248b6b1edfee582949
GlobuloBianco <wufrancescojh@gmail.com>
2025-03-20 15:27:00 +0000

    prova

--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 <?php
 include 'config.php';
 
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
